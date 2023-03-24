0570292df29d0ba25134aa30c6fa8f02d39c2a6a
LsdogodsL <bonnie33038@gmail.com>
2023-03-25 00:14:50 +0900

    main.py を更新
    Learning

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("hello world")
-print("hello world")
+import openai
+
+KEY = "secret API KEY"
+openai.api_key = KEY
+
+messages = [
+  {"hello" }
+]
+
+completion = openai.ChatCompletion.create(
+  model="GTP-3.5-turbo"
+  messages=messages
+)
+print(completion)
